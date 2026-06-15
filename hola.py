397332f39b5f8a10b864a11474e13934db5aba6b
Usuario <usuario@example.com>
2026-06-15 12:38:17 +0200

    Actualizar hola.py

--- hola.py
+++ hola.py
@@ -1,7 +1,3 @@
 print("Hola, este es mi primer archivo de python en visual estudio code")
 nombre = input("Ingrese su nombre: ")
 print("Hola, " + nombre + "! Bienvenido a Python en Visual Studio Code.")
-
-git init
-git add hola.py
-git commit -m "Agregar hola.py
